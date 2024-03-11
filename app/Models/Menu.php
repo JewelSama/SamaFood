657220b5da9156486c2694e09dce37b53aa4edd6
JewelSama <osafilejewel@gmail.com>
2024-03-11 16:59:37 +0100

    Init and DB Schema

---
+++ app/Models/Menu.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Menu extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'restaurant_id',
+        'name',
+        'description',
+        'price',
+    ];
+
+    public function restaurant()
+    {
+        return $this->belongsTo(Restaurant::class);
+    }
+}
