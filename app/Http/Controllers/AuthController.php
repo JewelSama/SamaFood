04867284775e145fa5296ef1ee39057eff178e1f
JewelSama <osafilejewel@gmail.com>
2024-03-12 16:58:05 +0100

    register

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    /**
+     * Create User
+     * @param Request $request
+     * @return User 
+     */
+    public function register(Request $request)
+    {   
+        $request->validate([
+            'username' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'email', 'max:255', 'lowercase', 'unique:'.User::class],
+            'password' => 'required|confirmed|min:6',
+            'address' => ['required', 'string'],
+            'phone_number' => ['required', 'string', 'max:255'],
+        ]);
+
+        try {
+
+            $user = User::create([
+                'username' => $request->username,
+                'email' => $request->email,
+                'password' => Hash::make($request->password),
+                'address' => $request->address,
+                'phone_number' => $request->phone_number,
+            ]);
+
+            $token = $user->createToken('Sama Food');
+
+            return response()->json([
+                'status' => 'success',
+                'token' => $token->plainTextToken,
+                'user' => $user
+            ], 201);
+
+        } catch(\Throwable $th){
+
+            throw $th;
+
+            $errors = [
+                'error' => [
+                    'Something went wrong, please try again.'
+                ]
+            ];
+
+            return response()->json([
+                'status' => 'error',
+                'message' => 'Something went wrong, please try again.',
+                'errors' => $errors
+            ], 500);
+        
+        }
+    }
+}
