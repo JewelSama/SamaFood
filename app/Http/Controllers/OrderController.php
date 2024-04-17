af9a0022352abf51eabc8efb740019deabe2acbc
JewelSama <osafilejewel@gmail.com>
2024-04-17 16:09:37 +0100

    email to vendor on new order creation

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -3,9 +3,11 @@
 namespace App\Http\Controllers;
 
 use App\Mail\OrderStatusMail;
+use App\Mail\NewOrderMail;
 use App\Models\Menu;
 use App\Models\Order;
 use App\Models\User;
+use App\Models\Vendor;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Mail;
 
@@ -63,6 +65,7 @@ class OrderController extends Controller
             // You can retrieve the vendor_id from any of the menu item
             $vendorId = $cartItems[0]['vendor_id'];
              // Assuming vendor_id is included in $cartItems
+            $vendor = Vendor::where('id', $vendorId)->first();
 
             $user=User::findOrFail(auth()->id());
 
@@ -100,13 +103,21 @@ class OrderController extends Controller
                 ]);
             }
 
+
+            $data = [
+                'title'=> "A new Order has been created",
+                'content' => 'Open the SamaFood app to learn more.'
+            ];
+         
+            Mail::to($vendor->email)->send(new NewOrderMail($data));
+
             
             
     
 
             return response()->json([
                 'status' => 'success',
-                'data' => $menuIds,
+                'data' => $vendor->email,
                 // 'pricee' => $totalPrice
             ], 201);
 
